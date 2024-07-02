90fffce379d88ef8fdf1a9562fb4400518378984
trai25 <trai25@gmail.com>
2024-07-02 23:21:00 +0200

    update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,16 @@
+'use client'
+
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import { codeInput } from '@sanity/code-input'
-import { visionTool } from '@sanity/vision'
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { media } from 'sanity-plugin-media'
-
-import { settingsPlugin, settingsStructure } from '~/sanity/plugins/settings'
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './sanity/env'
-import { schema } from './sanity/schema'
-import settingsType from './sanity/schemas/settings'
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schema'
 
 export default defineConfig({
   basePath: '/studio',
@@ -22,16 +19,9 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    structureTool({ structure: settingsStructure(settingsType) }),
+    structureTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
-    settingsPlugin({
-      type: settingsType.name,
-    }),
-    media(),
-    codeInput(),
+    visionTool({defaultApiVersion: apiVersion}),
   ],
 })
